4e3a237cb87524bcb2123cf81a7b25b3f7c8dd6d
igor-arkhipenko <i.arkhipenko@crystals.ru>
2020-07-16 13:59:08 +0300

    base types

---
+++ 1-types.ts
@@ -0,0 +1,17 @@
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'Hello TypeScript'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['Hello', 'TypeScript']
+
+// Tuple
+const contact: [string, number] = ['Igor', 89998887766]
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # typescript-course
-TypeScript course
+Обобщенный полный курс по typescript из youtube: https://www.youtube.com/watch?v=nyIpDs2DJ_c&t=314s
