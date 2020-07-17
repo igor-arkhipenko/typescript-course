0dea51107779d48f89013c3171df5211bc58e6b5
igor-arkhipenko <i.arkhipenko@crystals.ru>
2020-07-17 15:18:57 +0300

    field modificators

--- 5-classes.ts
+++ 5-classes.ts
@@ -23,3 +23,32 @@ class Car {
     readonly numberOfWheels: number = 4
     constructor(readonly model: string) {}
 }
+
+// ==========================================
+
+class Animal {
+    public color: string = 'black'
+    protected voice: string = ''
+    private go() {
+        console.log('Go!')
+    }
+
+    constructor() {
+        this.go()
+    }
+}
+
+const animal = new Animal()
+console.log(animal)
+
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice
+    }
+}
+
+const cat = new Cat()
+cat.setVoice('Meow')
+console.log(cat)
+// cat.voice    - недоступно напрямую для потомка класса Animal из-за protected.
+//              - можно задать только методом из класса-потомка (setVoice)
